4e63e39d98ac4f8d7557460e8cd9663a6e4dcf80
build <build@fake.com>
2017-11-06 14:09:20 -0500

    disable workspace integrity during chef run

--- shell/lib/tools/wrappers/chef.bash
+++ shell/lib/tools/wrappers/chef.bash
@@ -11,11 +11,14 @@ function chef__configure_workspace(){
   echo ""
   echo "$(colorize -t $LIGHT_CYAN 'beginning workspace configuration with chef, admin privilages required, you may be prompted...')"
   echo ""
-  
+
+  export VALIDATE_INTEGRITY=false
+
   sudo__execute_with_administrator_privileges chef-solo -c "${ECOSYSTEM_CHEF_HOME}/solo.rb" -j "${ECOSYSTEM_CHEF_HOME}/node.json"  "${*}"
   local chef_exit_code=$?
-   if [[ $chef_exit_code == 0 ]]; then
+  if [[ $chef_exit_code == 0 ]]; then
     set_integrity
+    export VALIDATE_INTEGRITY=true
   fi
   fail_if "Failed to configure workspace" $chef_exit_code
 }
