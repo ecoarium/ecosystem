0af6a111552b73990abab2dc90b638fe97a4495b
Jay Flowers <jay.flowers@gmail.com>
2017-11-07 21:38:52 -0500

    add function to map protocol name to number

--- ruby/cfndsl-ext/lib/cfndsl-ext/functions.rb
+++ ruby/cfndsl-ext/lib/cfndsl-ext/functions.rb
@@ -3,4 +3,17 @@ require 'cfndsl'
 
 module CfnDsl::Functions
 
+  def protocol_number(name)
+    protocols = {
+      ah:   51,
+  		esp:  50,
+  		udp:  17,
+  		tcp:  6,
+  		icmp: 1,
+  		all:  -1,
+  		vrrp: 112
+    }
+
+    protocols[name.to_sym]
+  end
 end
